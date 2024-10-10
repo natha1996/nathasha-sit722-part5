f4442e7d0aac45ce706033315055e638a8a08c5f
natha1996 <73653061+natha1996@users.noreply.github.com>
2024-10-10 12:32:22 +1100

    Update container-registry.tf

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -3,7 +3,7 @@
 #
 resource "azurerm_container_registry" "container_registry" {
   name                = var.container_registry
-  resource_group_name = azurerm_resource_group.s224169428.name
+  resource_group_name = azurerm_resource_group.flixtube.name
   location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
