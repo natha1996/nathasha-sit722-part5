c43d2697cbec00a45036e2fb7c7e3198f7395158
natha1996 <73653061+natha1996@users.noreply.github.com>
2024-10-10 14:42:51 +1100

    Update providers.tf

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -14,5 +14,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id = "42774068-bda7-489f-b6e0-2cc3e79a28ae" 
+  skip_provider_registration = true
   features {}
 }
