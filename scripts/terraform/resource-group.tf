fba58bc030aca56c64ad8e1f94fa83c73c014c11
natha1996 <73653061+natha1996@users.noreply.github.com>
2024-10-10 13:01:53 +1100

    Update resource-group.tf

--- scripts/terraform/resource-group.tf
+++ scripts/terraform/resource-group.tf
@@ -1,7 +1,7 @@
 #
 # Creates a resource group for FlixTube in your Azure account.
 #
-resource "azurerm_resource_group" "flixtube" {
+resource "azurerm_resource_group" "deakinuni" {
   name     = var.app_name
   location = var.location
 }
